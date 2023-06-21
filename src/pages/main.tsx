5df4538b696f45338d60fc9aead92b94cc10d9e5
Natawat <105378998+NatawatOhm@users.noreply.github.com>
2023-06-21 22:55:43 +0700

    Initial commit

---
+++ src/pages/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <div className="text-3xl flex justify-center ">This is main pages</div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/router/main/index.tsx
@@ -0,0 +1,10 @@
+import Main from '../../pages/main.tsx';
+
+const MainPage = [
+  {
+    path: '/main',
+    element: <Main />,
+  },
+];
+
+export default MainPage;
