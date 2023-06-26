569be8bf96ebef02c69e9a4e964856343616d438
NatawatOhm <natawat.cho@gmail.com>
2023-06-26 23:18:22 +0700

    fix show image type

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 
 const Main: React.FC = () => {
-  const [selectedImage, setSelectedImage] = useState<string>("");
+  const [selectedImage, setSelectedImage] = useState<File>();
+  const [imageUrl, setImageUrl] = useState<string>("");
 
   const handleInput = (e: any) => {
-    setSelectedImage(URL.createObjectURL(e.target.files[0]));
+    setSelectedImage(e.target.files[0]);
+    const objectUrl = URL.createObjectURL(e.target.files[0]);
+    setImageUrl(objectUrl);
+    console.log(e.target.files[0]);
   };
-
   return (
     <div>
       <div className="text-3xl flex justify-center">
         <input type="file" onChange={handleInput} />
       </div>
-      <img src={selectedImage} alt="" />
+      <img src={imageUrl} alt="" />
     </div>
   );
 };
